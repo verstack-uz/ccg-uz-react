fc45ee7d53e40e8f08d07ee1689fe876a13c515e
Qobiljon <qobiljon.toshnazarov@gmail.com>
2026-01-14 13:49:09 +0500

    feat: implement ProjectsCarousel component for project display

---
+++ src/components/elements/ProjectsCarousel.tsx
@@ -0,0 +1,54 @@
+import Autoplay from "embla-carousel-autoplay";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Projects from "@/lib/data/projects";
+import i18, { LanguageCode } from "@/i18n";
+
+export default function ProjectsCarousel() {
+  const projects = Projects.getInstance();
+  const langCode: LanguageCode = i18.language;
+
+  return (
+    <Carousel
+      opts={{
+        align: "center",
+        loop: true,
+      }}
+      plugins={[
+        Autoplay({
+          delay: 1500,
+          stopOnInteraction: false,
+        }),
+      ]}
+      className="w-full mt-8"
+    >
+      <CarouselContent>
+        {projects.map((project) => (
+          <CarouselItem key={project.id} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card className="overflow-hidden hover:shadow-xl transition-shadow duration-300 p-0">
+                <CardContent className="p-0">
+                  <img
+                    src={project.imageUrls[0]}
+                    alt={`Project ${project.id} Image`}
+                    className={"object-cover w-full h-64"}
+                  />
+                  <div className={"h-24 p-2 flex items-center justify-center"}>
+                    <span className={"text-xl text-center line-clamp-3"}>
+                      {project.title[langCode]}
+                    </span>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,60 +1,16 @@
 import { useTranslation } from "react-i18next";
-import Autoplay from "embla-carousel-autoplay";
 
-import { Card, CardContent } from "@/components/ui/card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
-import Projects from "@/lib/data/projects";
+import ProjectsCarousel from "@/components/elements/ProjectsCarousel";
 
 export default function HomePage() {
   const { t } = useTranslation();
-  const projects = Projects.getInstance();
 
   return (
     <div className={"mt-8 md:mt-0"}>
       <h1 className={"text-2xl md:text-4xl text-center tracking-normal"}>
         {t("Current projects").toUpperCase()}
       </h1>
-
-      <Carousel
-        opts={{
-          align: "center",
-          loop: true,
-        }}
-        plugins={[
-          Autoplay({
-            delay: 1500,
-            stopOnInteraction: false,
-          }),
-        ]}
-        className="w-full mt-8"
-      >
-        <CarouselContent>
-          {projects.map((project) => (
-            <CarouselItem
-              key={project.id}
-              className="md:basis-1/2 lg:basis-1/3"
-            >
-              <div className="p-1">
-                <Card className="overflow-hidden hover:shadow-xl transition-shadow duration-300 p-0">
-                  <CardContent className="p-0">
-                    <div className="aspect-square relative bg-linear-to-br from-gray-100 to-gray-200">
-                      <img
-                        src={project.imageUrls[0]}
-                        alt={`Project ${project.id} Image`}
-                        className="object-cover w-full h-full"
-                      />
-                    </div>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-      </Carousel>
+      <ProjectsCarousel />
     </div>
   );
 }
